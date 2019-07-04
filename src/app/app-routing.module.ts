3edca7e502f8c1f01693dab22ec9e7bdc1c640fc
Pelix Ng <nce2359@yahoo.com.sg>
2019-07-05 03:18:45 +0800

    join screen in progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,44 +5,54 @@ import { SignUpComponent } from './authentication/sign-up/sign-up.component';
 import { CoverComponent } from './cover/cover.component';
 import { NavbarComponent } from './main/navbar/navbar.component';
 import { CoverContentComponent } from './cover-content/cover-content.component';
-import {AuthenticationComponent} from './authentication/authentication.component';
-import {ForgetPasswordComponent} from './authentication/forget-password/forget-password.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { ForgetPasswordComponent } from './authentication/forget-password/forget-password.component';
 
-import {MainComponent} from './main/main.component';
+import { MainComponent } from './main/main.component';
 import { HomeComponent } from './main/home/home.component';
 import { ProfilePageComponent } from './main/profile-page/profile-page.component';
+import { GameComponent } from './game/game.component';
+import { JoinComponent } from './game/join/join.component';
 
 const routes: Routes = [
-  
+
   {
-    path: '', 
-    component:CoverComponent,
+    path: '',
+    component: CoverComponent,
     children: [
-      {path: 'cover', component: CoverContentComponent},
+      { path: 'cover', component: CoverContentComponent },
       {
         path: 'authentication',
         component: AuthenticationComponent,
 
         children: [
-          {path: 'login', component:LoginComponent},
-          {path: 'sign-up', component:SignUpComponent},
-          {path: 'forget-password', component:ForgetPasswordComponent}
+          { path: 'login', component: LoginComponent },
+          { path: 'sign-up', component: SignUpComponent },
+          { path: 'forget-password', component: ForgetPasswordComponent }
         ],
 
       }
     ]
 
-    
+
   },
 
   {
     path: 'main',
-    component:MainComponent,
+    component: MainComponent,
     children: [
-      {path: 'home', component: HomeComponent},
-      {path: 'profile-page', component:ProfilePageComponent},
+      { path: 'home', component: HomeComponent },
+      { path: 'profile-page', component: ProfilePageComponent },
     ]
-    
+
+  },
+  {
+    path: 'game',
+    component: GameComponent,
+
+    children: [
+      { path: 'join', component: JoinComponent }
+    ]
   }
 ];
 
@@ -51,4 +61,5 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [CoverComponent,AuthenticationComponent,LoginComponent,SignUpComponent,ForgetPasswordComponent,MainComponent,HomeComponent,ProfilePageComponent]
+export const routingComponents = [CoverComponent, AuthenticationComponent, LoginComponent, SignUpComponent, ForgetPasswordComponent, MainComponent, HomeComponent,
+  ProfilePageComponent, GameComponent, JoinComponent]
