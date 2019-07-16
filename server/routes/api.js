fa1b17ac698d6fc46776928a3da787876363f77f
Pelix Ng <nce2359@yahoo.com.sg>
2019-07-16 22:30:59 +0800

    Able to upload pfp placeholder

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,37 +5,64 @@ const axios = require('axios');
 const API = 'https://jsonplaceholder.typicode.com';
 // get API listing
 router.get('/', (req, res) => {
-res.send('api works');
+    res.send('api works');
 });
 
 
-   
+
 //Connect to MongoDB
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectID;
 var db;
 MongoClient.connect('mongodb+srv://Pelix-Ng:tB776773@pequizcluster-ndlzr.mongodb.net/test?retryWrites=true&w=majority', {
-useNewUrlParser: true }, (err, database) => {
- if (err) return console.log(err);
- db = database.db('PeQuizDB');
+    useNewUrlParser: true
+}, (err, database) => {
+    if (err) return console.log(err);
+    db = database.db('PeQuizDB');
+});
+
+
+
+// route middleware that will happen on every request
+router.use(function (req, res, next) {
+
+    // log each request to the console
+    console.log(req.method, req.url);
+
+    // continue doing what we were doing and go to the route
+    next();
 });
 
 // Get all posts
-router.route('/users/').get(function(req, res) {
-    db.collection('User').find().toArray( (err, results) =>
-   {res.send(results)});
-   });
-   
+router.route('/users/').get(function (req, res) {
+    db.collection('User').find().toArray((err, results) => { res.send(results) });
+});
+
 
 
 // register new user
+//called for any requests passed onto this 'router' object, req.body contains the data.
 router.route('/users/').post(function (req, res) {
- db.collection('User').insertOne(req.body, (err, results) => {
- if (err) return console.log(err);
- console.log('saved to database');
- res.send(results);
- });
+
+    fs.readFile('src/assets/images/pfp_placeholder.png', 'utf8', function (err, contents) {
+
+        pfpPlaceHolder = (Buffer.from(contents).toString('base64'));
+
+        console.log(pfpPlaceHolder);
+        var reqMsg = req.body;
+        reqMsg["profile_picture"] = pfpPlaceHolder;
+        db.collection('User').insertOne(reqMsg, (err, results) => {
+            if (err) return console.log(err);
+            console.log('saved to database');
+            res.send(results);
+        });
+    });
 });
+
+
+var fs = require('fs');
+var pfpPlaceHolder;
+
 
 
 
--- src/app/services/user/user-post.service.ts
+++ src/app/services/user/user-post.service.ts
@@ -15,8 +15,7 @@ export class UserPostService {
 
  registerUser (username: string,email: string, password: string
   // total_quizzes_created: number, last_password_change: Date, online_status: boolean, friends_list: string, created_quizzes: string
-  ) {
-    console.log(username);
+  ) {  
   return this.http.post<any[]>('./api/users/', {'username': username, 'email': email, 'password': password
   //  'last_password_change': last_password_change, 'online_status': online_status, 'friends_list': friends_list, 'created_quizzes': created_quizzes
   });
