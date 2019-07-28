5cbfaf934d842dc96f4ce28495e3e171747971a9
Pelix Ng <nce2359@yahoo.com.sg>
2019-07-28 13:44:43 +0800

    Checkpoint b4 web socket

--- src/app/game/join/join.component.ts
+++ src/app/game/join/join.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserGetService } from 'src/app/services/user/user-get.service';
 
 @Component({
   selector: 'app-join',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JoinComponent implements OnInit {
 
-  constructor() { }
+  public user_data;
+  public user_id;
+  
+  
+
+  constructor(private userGetService: UserGetService,private authService: AuthService,private router: Router) { }
 
   ngOnInit() {
+
+    this.user_id = this.authService.getSecureToken()
+    console.log(this.user_id)
+
+    this.findUserById()
+  }
+
+  findUserById(){
+    this.userGetService.getUserById(this.user_id).subscribe(data => {
+        this.user_data = data;
+
+        console.log(this.user_data);
+
+        this.userGetService.changeData(this.user_data)
+
+
+
+
+      });
+  }
+
+  routeToHome(){
+    console.log("hi")
+    this.router.navigate(["/main/home/" + this.user_id])
   }
 
 }
